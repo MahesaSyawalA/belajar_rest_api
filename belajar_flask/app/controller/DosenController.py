8dec8b32083a77db8c26002c65663a0c4f48673a
MahesaSyawalA <mahesaabd4@gmail.com>
2025-01-23 11:45:49 +0700

    create route for detail dosen and display detail data

--- belajar_flask/app/controller/DosenController.py
+++ belajar_flask/app/controller/DosenController.py
@@ -1,4 +1,5 @@
 from app.model.dosen import Dosen
+from app.model.mahasiswa import Mahasiswa
 
 from app import response, app, db
 from flask import request
@@ -22,10 +23,59 @@ def formatArray(datas):
 def singleObject(data):
     data = {
         'id':data.id,
-        'idn': data.nidn,
+        'nidn': data.nidn,
         'nama': data.name,
         'phone': data.phone,
         'alamat': data.alamat,
     }
 
     return data
+
+def detail(id):
+    try:
+        dosen = Dosen.query.filter_by(id=id).first()
+        mahasiswa = Mahasiswa.query.filter((Mahasiswa.dosen_satu == id) | (Mahasiswa.dosen_dua == id))
+
+        if not dosen:
+            return response.badRequest([],'Not Found')
+        
+        dataMahasiswa = formatMahasiswa(mahasiswa)
+        data = singleDetailMahasiswa(dosen,dataMahasiswa)
+
+        return response.success(data,'success')
+    
+    except Exception as e:
+        print(e)
+    
+def singleDetailMahasiswa(dosen, mahasiswa):
+    data = {
+        'id' : dosen.id,
+        'nidn':dosen.nidn,
+        'nama':dosen.name,
+        'phone':dosen.phone,
+        'alamat':dosen.alamat,
+        'mahasiswa':mahasiswa,
+    }
+
+    return data
+
+
+
+
+def singleMahasiswa(mahasiwa):
+    data = {
+        'id':mahasiwa.id,
+        'nim': mahasiwa.nim,
+        'nama': mahasiwa.name,
+        'phone': mahasiwa.phone,
+        'alamat': mahasiwa.alamat,
+    }
+
+    return data
+
+def formatMahasiswa(data):
+    array = []
+    for i in data:
+        array.append(singleMahasiswa(i))
+
+    return array
